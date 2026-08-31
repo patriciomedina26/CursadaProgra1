d3746eac7b1635d0edc85560af843d80c4e3bd14
Patricio Medina <pato_medina20@hotmail.com>
2026-08-31 00:46:00 -0300

    Arranque con ejercicioPracticas1

---
+++ src/introduccion/EjercicioPracticas1.java
@@ -0,0 +1,19 @@
+package introduccion;
+
+import java.util.Scanner;
+
+public class EjercicioPracticas1 {
+
+	public static void main(String[] args) {
+	
+	}
+	public static String pedirPalabra() {
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Ingrese su palabra: ");
+		return scan.nextLine();
+		}
+	public static int contadorVocales(String palabra) {
+		String vocales="AEIOUaeiou";
+		
+	}
+}
