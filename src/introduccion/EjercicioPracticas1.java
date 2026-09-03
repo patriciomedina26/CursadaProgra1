578efdb40df1f829ce83894c0cdf9b282b924262
Patricio <pato_medina20@hotmail.com>
2026-09-03 10:58:42 -0300

    Comienzo Ejercicio practicas 1 y eliminacion de ejercicios basura

--- src/introduccion/EjercicioPracticas1.java
+++ src/introduccion/EjercicioPracticas1.java
@@ -5,7 +5,9 @@ import java.util.Scanner;
 public class EjercicioPracticas1 {
 
 	public static void main(String[] args) {
-	
+		String palabraElegida=pedirPalabra();
+		int cantidadVocales=contadorVocales(palabraElegida);
+		System.out.println("La palabra " + palabraElegida + " tiene " + cantidadVocales + " vocales.");
 	}
 	public static String pedirPalabra() {
 		Scanner scan=new Scanner(System.in);
@@ -13,7 +15,12 @@ public class EjercicioPracticas1 {
 		return scan.nextLine();
 		}
 	public static int contadorVocales(String palabra) {
+		int contador = 0;
 		String vocales="AEIOUaeiou";
-		
+		for (int i=0;i<palabra.length();i++) {
+			if (vocales.indexOf(palabra.charAt(i))!=-1)
+				contador++;
+		}
+		return contador;
 	}
 }
