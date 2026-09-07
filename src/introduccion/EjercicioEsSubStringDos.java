85889ce7009db8582683bea6624992682d993031
Patricio <pato_medina20@hotmail.com>
2026-09-07 13:11:21 -0300

    Falta 1 solo ejercicio, el de palindrome

--- src/introduccion/EjercicioEsSubStringDos.java
+++ src/introduccion/EjercicioEsSubStringDos.java
@@ -2,36 +2,35 @@ package introduccion;
 
 import java.util.Scanner;
 
-public class EjercicioEsSubString {
+public class EjercicioEsSubStringDos {
 
 	public static void main(String[] args) {
 		String palabrauno=pedirPalabra();
 		String palabrados=pedirPalabra();
 		boolean valor=esSubString(palabrauno,palabrados);
 		System.out.println(valor);
+
 	}
-	
+
 	public static String pedirPalabra() {
 		Scanner scan=new Scanner(System.in);
 		System.out.println("Ingrese su palabra: ");
 		return scan.nextLine();
 	}
 	
-	public static boolean esSubString(String palabrauno,String palabrados) {
+	public static boolean esSubString(String palabrauno, String palabrados) {
 		if (palabrauno.length()>palabrados.length())
 			return false;
 		for (int pos=0;pos<=palabrados.length()-palabrauno.length();pos++) {
-			boolean coincide = true;
-			for (int i=0;i<palabrauno.length();i++) {
-				if (palabrauno.charAt(i)!=palabrados.charAt(pos+i)) {
-					coincide = false;
-					break;
-				}
-			}
-			if (coincide) {
-				return true;
+			int contadorCoincidencias=0;
+					for (int i=0;i<palabrauno.length();i++) {
+						if (palabrauno.charAt(i)==palabrados.charAt(pos+i)) {
+							contadorCoincidencias++;	
+						}
+					}
+					if (contadorCoincidencias==palabrauno.length())
+						return true;
 		}
-		}	
 		return false;
 	}
 }
